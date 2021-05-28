27e6815d088ff9be9c191505b5a90e03b3c77d34
aditya635 <55912563+aditya635@users.noreply.github.com>
2021-05-29 01:18:43 +0530

    Djano urls and views

--- Back-End/monthly_challenges/challenges/urls.py
+++ Back-End/monthly_challenges/challenges/urls.py
@@ -3,6 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [ 
-    path("january",views.index),
-      path("february",views.index1)
+  #  path("january",views.index),
+  #    path("february",views.index1),
+  path("<int:month>",views.monthly_challenges),#<> is used for dynamic paths
+  path("<str:month>",views.monthly_challenge, name="dad")#value added here with str: should be used as a string
+ 
 ]
--- Back-End/monthly_challenges/challenges/views.py
+++ Back-End/monthly_challenges/challenges/views.py
@@ -1,11 +1,41 @@
+from django.http.response import HttpResponseNotFound
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse 
 
 
+monthly_challenging={
+    "january":"Start Working",
+    "february":"Start Crying",
+    "march":"Start Living",
+    "april":"Start Rolling"
+}
 # Create your views here.
 
 
-def index(request):
-    return HttpResponse("This Januray")
-def index1(request):
-    return HttpResponse("This February")
+#def index(request):
+  #  return HttpResponse("This Januray")
+
+#def index1(request):
+    #return HttpResponse("This February")
+
+
+#def monthly_challenge(request,month):
+#    if month=="january":
+#        return HttpResponse("This is "+month+" Sleep!")
+#    elif month=="february":
+#        return HttpResponse("This is "+month+" love")
+#    else:
+#        return HttpResponseNotFound("Bad month")
+def monthly_challenge(request,month):
+    try:
+        vro = monthly_challenging[month]
+        return HttpResponse(vro)
+    except:
+        return HttpResponseNotFound("Shit ain't found!")
+def monthly_challenges(request,month):
+    forward=list(monthly_challenging.keys())
+    forward_month=forward[month-1]
+    newurl = reverse("dad", args = [forward_month] )  #we make /challenge/januray with this,basically we /challenge/ is preset we can't change it in response redirect like below
+    #return HttpResponseRedirect("/challenges/+forward_month")
+    return HttpResponseRedirect(newurl)
