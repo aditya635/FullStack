8f58391722fe3afef64bf3dd4ca77404a835938a
aditya635 <55912563+aditya635@users.noreply.github.com>
2021-05-31 09:47:58 +0530

    worked smh

--- Back-End/monthly_challenges/challenges/urls.py
+++ Back-End/monthly_challenges/challenges/urls.py
@@ -7,5 +7,8 @@ urlpatterns = [
   #    path("february",views.index1),
   path("<int:month>",views.monthly_challenges),#<> is used for dynamic paths
   path("<str:month>",views.monthly_challenge, name="dad")#value added here with str: should be used as a string
+  #now to give something for only the /challenges and no extra request parameter with it
+  ,
+  path("",views.index)
  
 ]
--- Back-End/monthly_challenges/challenges/views.py
+++ Back-End/monthly_challenges/challenges/views.py
@@ -2,13 +2,14 @@ from django.http.response import HttpResponseNotFound
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse 
-
+from django.template.loader import render_to_string
 
 monthly_challenging={
     "january":"Start Working",
     "february":"Start Crying",
     "march":"Start Living",
     "april":"Start Rolling"
+    ,"december": None
 }
 # Create your views here.
 
@@ -30,12 +31,28 @@ monthly_challenging={
 def monthly_challenge(request,month):
     try:
         vro = monthly_challenging[month]
-        return HttpResponse(vro)
+        #responsedata=f"<h1>{vro}</h1>"
+        #responsedata = render_to_string("challenges/challenge.html")
+        #return HttpResponse(responsedata)
+        #return render(request, "challenges/challenge.html" ) can't send dynamic data without 3rd arguement
+        return render(request, "challenges/challenge.html", {"text" : vro, "dad": month} )
     except:
-        return HttpResponseNotFound("Shit ain't found!")
+        return HttpResponseNotFound("<h1>Shit ain't found!</h1>")
+
+
 def monthly_challenges(request,month):
     forward=list(monthly_challenging.keys())
     forward_month=forward[month-1]
     newurl = reverse("dad", args = [forward_month] )  #we make /challenge/januray with this,basically we /challenge/ is preset we can't change it in response redirect like below
     #return HttpResponseRedirect("/challenges/+forward_month")
     return HttpResponseRedirect(newurl)
+
+def index(request):
+    #listitems=""
+    forward=list(monthly_challenging.keys())
+    #for month in forward:
+    #    cmonth=month.capitalize()
+    #    monthpath = reverse("dad", args = [month] )
+    #    listitems += f"<li><a href=\"{monthpath}\">{cmonth}</a></li>" #we used \with other double quotes as to not confuse interpretor with which quotes is for html and which for the string part
+    #responsedata=f"<ul>{listitems}</ul>"
+    return render(request, "challenges/index.html", {"forward": forward})
